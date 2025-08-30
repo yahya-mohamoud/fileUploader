7eed6ff33cb124c61b1ffc329d07fc6defc112d4
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-08-30 11:10:31 +0300

    added a skeleton animations

--- public/script.js
+++ public/script.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // spinner loader
   const loadingOverlay = document.getElementById("loading-overlay");
   const content = document.getElementById("content");
 
 
-    loadingOverlay.style.display = "none";
-    content.classList.remove("content-hidden");
- 
+  // skeleton loader
+  const skeleton = document.querySelectorAll('.bucket')
+  const fileContent = document.querySelectorAll('#content')
+
+    skeleton.forEach(el => {
+      el.style.display = "none"
+    })
+    content.classList.remove("hidden");
+
+    fileContent.forEach(elm => {
+      elm.classList.remove("hidden");
+    })
+
 });
