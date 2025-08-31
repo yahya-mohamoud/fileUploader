52cb58f8f890b577c7686b8bd681948def4db355
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-08-31 07:50:36 +0300

    deplyment

--- supabase.js
+++ supabase.js
@@ -1,9 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from 'dotenv'
-dotenv.config()
+
+if(process.env.NODE_ENV === "production") {
+    dotenv.config()
+}
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SERVICE_ROLE_KEY;
 
+<<<<<<< HEAD
 console.log(supabaseUrl)
+=======
+console.log("url", supabaseUrl, " end")
+console.log("jey", supabaseKey, " end")
+>>>>>>> 4e028cf (Deployment commit.)
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
