8e8acb10d7a6278a0649024e93704f9bfb169a63
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-08-31 07:53:35 +0300

    resolved merge confclict

--- supabase.js
+++ supabase.js
@@ -7,11 +7,4 @@ if(process.env.NODE_ENV === "production") {
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SERVICE_ROLE_KEY;
 
-<<<<<<< HEAD
-console.log(supabaseUrl)
-=======
-console.log("url", supabaseUrl, " end")
-console.log("jey", supabaseKey, " end")
->>>>>>> 4e028cf (Deployment commit.)
-
 export const supabase = createClient(supabaseUrl, supabaseKey)
